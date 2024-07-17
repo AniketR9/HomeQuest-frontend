fdabdd9129e19ae8f00bc4530809dada35d71e1c
AniketR9 <rajeshaniket13@gmail.com>
2024-07-17 16:19:13 +0530

    Finished basic home page

---
+++ src/routes/homepage/homepage.jsx
@@ -0,0 +1,19 @@
+import './homepage.scss'
+
+function HomePage(){
+ return (
+    <div className='homePage'>
+        <div className="textContainer">
+            <div className="wrapper">
+            <h1 className='title'>Find Real Estate & Get Your Dream Place Today</h1>
+            <p>dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio</p>
+        </div>
+        </div>
+        <div className="imgContainer">
+            <img src="/bg.png" alt="" />
+        </div>
+    </div>
+ )
+}
+
+export default HomePage
