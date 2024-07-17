42a3d21b818e75fa2968e681875156bb5a486e87
AniketR9 <rajeshaniket13@gmail.com>
2024-07-17 15:52:46 +0530

    Done with the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Navbar from "./components/navbar/Navbar"
+import Navbar from "./components/navbar/Navbar";
 import "./layout.scss";
-
 
 function App() {
   return (
     <div className="layout">
       <Navbar />
-      </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import "./navbar.scss"
+import React, { useState } from "react";
+import "./navbar.scss";
 
-function Navbar(){
-return <nav>
-    <div className='left'>
-        <a href="/" className='logo'>
-            <img src="/logo.png" alt="" />
-            <span>HomeQuest</span>
+function Navbar() {
+    const [open,setOpen]=useState(false);
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>HomeQuest</span>
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>
         <a href="/">Contact</a>
         <a href="/">Agents</a>
-    </div>
-    <div className='right'>
-    <a href="/">Sign In</a>
-    <a href="/" className='register'>Sign Up</a>
-    </div>
-</nav>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/" className="register">
+          Sign up
+        </a>
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" onClick={()=>setOpen(!open)} />
+        </div>
+        <div className={open? "menu active":"menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-
-export default Navbar
+export default Navbar;
